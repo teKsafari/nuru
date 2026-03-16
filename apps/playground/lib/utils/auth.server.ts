242e648988071e1f12c26cbf137a902fe24fc944
Heracraft <heracraftgamer.ne@gmail.com>
2026-03-15 21:49:51 -0400

    feat(auth): add server-only auth utilities

---
+++ apps/playground/lib/utils/auth.server.ts
@@ -0,0 +1,32 @@
+import "server-only";
+
+import type {
+	IdTokenClaims,
+	LogtoContext,
+	UserInfoResponse,
+} from "@logto/next";
+
+import { AuthSession, AuthContextType } from "@/types/auth";
+
+// auth utilities for the server only
+
+export function getContextFromClaims(
+	isAuthenticated: boolean,
+	claims: IdTokenClaims | null,
+): AuthContextType {
+	let context: AuthContextType;
+
+	if (isAuthenticated) {
+		context = {
+			isAuthenticated,
+			claims: (claims as AuthSession) || null,
+		};
+	} else {
+		context = {
+			isAuthenticated,
+			claims: null,
+		};
+	}
+
+	return context;
+}
