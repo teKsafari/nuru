b2a0abf3b4943ad62d8fb4400ca5f463d7bdc9d8
Heracraft <heracraftgamer.ne@gmail.com>
2026-04-01 15:35:41 -0400

    save point

--- apps/playground/next.config.mjs
+++ apps/playground/next.config.mjs
@@ -1,14 +1,18 @@
 // import { config } from "dotenv";
 // config({ path: "../../.env" });
 
+import withBundleAnalyzer from "@next/bundle-analyzer"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	typescript: {
-		ignoreBuildErrors: true,
+		ignoreBuildErros: true,
 	},
 	images: {
 		unoptimized: true,
 	},
 	allowedDevOrigins: ["10.0.0.111"],
 };
-export default nextConfig;
+export default withBundleAnalyzer({
+	enabled:process.env.ANALYZE === 'true',
+})(nextConfig);
