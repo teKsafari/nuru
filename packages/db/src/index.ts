96d1d946ea335ecbfce7dde95ed31b3ef207bb50
Heracraft <nehemiahelibariki@gmail.com>
2026-07-02 13:08:57 -0400

    feat: basic local postgres docker compose, env based db connections, seed data

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,38 @@
 // import "server-only"
 
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import { Client } from "pg";
+import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
+import { drizzle as pgDrizzle, NodePgClient } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const connectionString = process.env.DATABASE_URL!;
+
+type DB = ReturnType<typeof neonDrizzle<typeof schema>> | ReturnType<typeof pgDrizzle<typeof schema>>;
+
+const dbSingleton = globalThis as unknown as {
+	db: undefined | DB;
+};
+
+if (!dbSingleton.db) {
+	if (connectionString?.includes("neondb_owner")) {
+		console.log("Detected neon conneciton string, connecting to remote neon db");
+		dbSingleton.db = neonDrizzle(connectionString, { schema });
+	} else {
+		const localDBPassword = process.env.DEV_POSTGRES_PASSWORD;
+		if (!localDBPassword) {
+			throw new Error("Neither neon connection string nor local db config not found. Cannot establish  connection to db. Set either DATABASE_URL or DEV_POSTGRES_PASSWORD");
+		}
+		console.log("Connecting to local postgres db");
+		dbSingleton.db = pgDrizzle(`postgres://postgres:${localDBPassword}@localhost:5432/postgres`, { schema });
+	}
+}
 
 export * from "./schema";
-export const db = drizzle(sql, { schema });
+// export const db = neonDrizzle(connectionString, { schema });
+
+// export const db = pgDrizzle(connectionString, { schema });
+
+let db = dbSingleton.db;
+
+export { db };
